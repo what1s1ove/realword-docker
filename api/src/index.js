473898a6b53fadb84d06e15d409bccb52a3bbde1
what1s1ove <zubko.2007@gmail.com>
2020-10-12 07:54:32 +0300

    master: + env variables

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,14 @@
 const express = require(`express`)
 const app = express()
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
+const HOST = process.env.HOST
 
 app.get(`/test`, (_, res) => {
   res.send(`Our api server is working correctly`)
 })
 
 app.listen(PORT, () => {
-  console.log(`Started api services`)
+  console.log(`Started api services on port: ${PORT}`)
+  console.log(`On host ${HOST}`)
 })
