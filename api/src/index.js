318ef87e68f4041d6010a03f11a840ae56bd7342
what1s1ove <zubko.2007@gmail.com>
2020-10-13 09:16:13 +0300

    master: + save entity to the base

--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,41 @@
 const express = require(`express`)
+const mongoose = require(`mongoose`)
 const { connedDb } = require(`./helpers/db`)
 const { host, port, db } = require(`./configurations`)
 
 const app = express()
+
+const postSchema = new mongoose.Schema({
+  name: String,
+})
+
+const Post = mongoose.model(`Post`, postSchema)
 
 const startServer = () => {
   app.listen(port, () => {
     console.log(`Started api services on port: ${port}`)
     console.log(`On host ${host}`)
     console.log(`Our database ${db}`)
+
+    Post.find((err, posts) => {
+      if (err) {
+        console.log(err)
+      }
+
+      console.log('posts', posts)
+    })
+
+    const silence = new Post({
+      name: `Silence`,
+    })
+
+    silence.save((err, savedPost) => {
+      if (err) {
+        console.log(err)
+      }
+
+      console.log(`Saved post`, savedPost)
+    })
   })
 }
 
