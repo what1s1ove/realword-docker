9b54093440336f750d47b0be6fff38d1eb971501
what1s1ove <zubko.2007@gmail.com>
2020-10-15 20:21:35 +0300

    master: + front-end dev mod

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. Change from docker with volumes
+          We are in dev!
         </p>
         <a
           className="App-link"
